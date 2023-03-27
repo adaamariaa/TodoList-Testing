3474d3048c570f9a412d810c8d9c795e85231d44
Ada Meriläinen <ada.merilainen@gmail.com>
2023-03-27 15:06:07 +0300

    Added clear button and test for it

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,40 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 test('app todo', () => {
-render(<App />);
-const desc = screen.getByPlaceholderText(/Description/i);
-const date = screen.getByPlaceholderText(/date/i)
+  render(<App />);
+  const desc = screen.getByPlaceholderText(/Description/i);
+  const date = screen.getByPlaceholderText(/date/i)
 
-fireEvent.change(desc, { target: { value: "Go to coffee"}});
-fireEvent.change(date, { target: { value: "24.03.2023"}});
+  fireEvent.change(desc, { target: { value: "Go to coffee"}});
+  fireEvent.change(date, { target: { value: "24.03.2023"}});
 
-const button = screen.getByText(/add/i);
-fireEvent.click(button);
+  const addButton = screen.getByText(/add/i);
+  fireEvent.click(addButton);
 
 
-const tablecell = screen.getByText(/go to coffee/i)
-expect(tablecell).toBeInTheDocument();
+  const addingTablecell = screen.getByText(/go to coffee/i)
+  expect(addingTablecell).toBeInTheDocument();
 
+});
+
+test('clear todos', () => {
+  render(<App />);
+  const desc = screen.getByPlaceholderText(/Description/i);
+  const date = screen.getByPlaceholderText(/date/i)
+
+  fireEvent.change(desc, { target: { value: "Go to coffee"}});
+  fireEvent.change(date, { target: { value: "24.03.2023"}});
+
+  const addButton = screen.getByText(/add/i);
+  fireEvent.click(addButton);
+
+
+  const addingTablecell = screen.getByText(/go to coffee/i)
+  expect(addingTablecell).toBeInTheDocument();
+
+  const clearButton= screen.getByText(/clear/i);
+  fireEvent.click(clearButton);
+
+
+  expect(desc).toHaveValue("");
 });
