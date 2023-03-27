275d04284278af5062e99bdd6e27d6ba8d9c4afd
Ada Meriläinen <ada.merilainen@gmail.com>
2023-03-27 14:21:25 +0300

    Tests done

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+test('app todo', () => {
+render(<App />);
+const desc = screen.getByPlaceholderText(/Description/i);
+const date = screen.getByPlaceholderText(/date/i)
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+fireEvent.change(desc, { target: { value: "Go to coffee"}});
+fireEvent.change(date, { target: { value: "24.03.2023"}});
+
+const button = screen.getByText(/add/i);
+fireEvent.click(button);
+
+
+const tablecell = screen.getByText(/go to coffee/i)
+expect(tablecell).toBeInTheDocument();
+
 });
